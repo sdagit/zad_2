077b2a32f1049a560c42c770fee6f75840e775f2
Karina Wilk desktop <sda_git@protonmail.com>
2018-10-08 20:08:20 +0200

    Odejmowanie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,23 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        System.out.println("Podaj znak: + lub -");
+        char sign = input.next().charAt(0);
 
         System.out.println("Podaj 2 liczby a ja wyśweitlę ich sumę!");
         int a = input.nextInt();
         int b = input.nextInt();
 
-        System.out.println("SUMA:" + (a+ b));
+        if(sign == '+') {
+            System.out.println("SUMA:" + (a+ b));
+        } else if(sign == '-') {
+            System.out.println("RÓŻNICA:" + (a - b));
+        }else {
+            System.out.println("Nieobsługiwany znak!");
+        }
+
+
+
+
     }
 }
